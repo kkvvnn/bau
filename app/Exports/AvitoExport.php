7d037286d72e41d1b1a9c9c47037dc2ee67f1842
kkvvnn <kkvvnn89@gmail.com>
2023-04-24 17:36:26 +0300

    download file avito2

--- app/Exports/AvitoExport.php
+++ app/Exports/AvitoExport.php
@@ -6,9 +6,22 @@ namespace App\Exports;
 use App\Models\Product;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
+use Maatwebsite\Excel\Concerns\ToModel;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
+use PhpOffice\PhpSpreadsheet\Cell\DefaultValueBinder;
 
-class AvitoExport implements FromView
+class AvitoExport extends DefaultValueBinder implements FromView, WithCustomValueBinder
 {
+    public function bindValue(Cell $cell, $value)
+    {
+
+        $cell->setValueExplicit($value, DataType::TYPE_STRING);
+
+        return true;
+    }
+
     public function view(): View
     {
         return view('exports.avito', [
