c5e755be2a80760f590cfd22a59a1ea2dbe30ae0
kkvvnn <kkvvnn89@gmail.com>
2023-06-02 15:46:49 +0300

    fdrt34tdsd4

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -15,7 +15,7 @@ class PhotoController extends Controller
 
         $date = date("Y-m-d_His");
 
-        $image = $request->file('foto')->store('/storage/Foto/'.$path);
+        $image = $request->file('foto')->store('public/Foto/'.$path);
         return redirect('/');
     }
 }
