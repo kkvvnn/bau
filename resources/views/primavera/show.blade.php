a0c996503086c8bb526749230763aee12ae47e92
kkvvnn <kkvvnn89@gmail.com>
2023-06-08 19:25:09 +0300

    avito export primavera add

--- resources/views/primavera/show.blade.php
+++ resources/views/primavera/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<!-- <section class="py-5 text-center container">
+    <!-- <section class="py-5 text-center container">
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
         <h1 class="fw-light">Album example</h1>
@@ -19,126 +19,113 @@
 
 
 
-<div class="album py-5 bg-body-tertiary">
-    <div class="container">
-        <div>
-            <p></p>
-        </div>
-
-
-
-
-
-
-
-        <div class="row">
-
-            <div class="col">
-                <h2>{{$product->title}}</h2>
-                <hr>
-                <h3>
-                    PrimaVera
-                </h3>
-{{--                <h4>Коллекция:--}}
-{{--                    @foreach ($collection as $one_collection)--}}
-{{--                    <a href="/collection/{{$one_collection->Collection_Id}}" class="link-dark text-decoration-none text-reset">{{$one_collection->Collection_Name}}. </a>--}}
-{{--                    @endforeach--}}
-{{--                </h4>--}}
-                <h5>
-                    Цена: {{$product->price}} ₽
-                    <br>
-{{--                    <!-- Цена зак: {{$product->Price}} ₽/{{$product->MainUnit}} -->--}}
-{{--                    <!-- <br> {{round(($product->RMPrice/$product->Price)*100 - 100, 2)}}% -->--}}
-                </h5>
-                <hr>
-{{--                <h5>--}}
-{{--                    Остаток: {{$product->balanceCount}} {{$product->MainUnit}}--}}
-{{--                </h5>--}}
-{{--                <hr>--}}
-                <h5>
-                    Артикул: {{$product->vendor_code}}
-                </h5>
-                <hr>
-                <h5>В упаковке шт: <strong>{{$product->count_in_pack}}</strong>   кв.м: <strong>{{$product->meters_in_pack}}</strong></h5>
-                <hr>
-{{--                <div id="app-5">--}}
-{{--                    <input v-model="count" placeholder="Количество кв.м?"> --}}
-{{--                    --}}
-{{--                    <h5>@{{ packages }} упаков. общая площадь @{{ all }} кв.м</h5>--}}
-{{--                    --}}
-{{--                </div>--}}
-{{--                <hr>--}}
+    <div class="album py-5 bg-body-tertiary">
+        <div class="container">
+            <div>
+                <p></p>
             </div>
-        </div>
-{{--        <!-- <div class="container">--}}
-{{--                <div class="row" data-masonry='{"percentPosition": true }'>--}}
-{{--                    <div class="col-sm-6 col-lg-4 mb-4">--}}
-
-{{--                        @foreach ($url_collection as $url_c)--}}
-{{--                        <div>--}}
-{{--                            <img src="/storage/Collections/{{$url_c}}" class="img-fluid shadow p-3 mb-5 bg-white rounded" alt="...">--}}
-{{--                        </div>--}}
-{{--                        @endforeach--}}
-
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div> -->--}}
-{{--        <!-- <hr> -->--}}
-{{--        <!-- <div class="row" data-masonry='{"percentPosition": true }'>--}}
-{{--                <div class="col-sm-6 col-lg-4 mb-4">--}}
-{{--                    <div>--}}
-{{--                        @foreach ($urls as $url)--}}
-{{--                        <img src="{{$url}}" class="img-fluid shadow p-3 mb-5 bg-white rounded" alt=".">--}}
-{{--                        @endforeach--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div> -->--}}
 
 
+            <div class="row">
 
-
-
-        <div class="container-fluid">
-
-
-
-            <div class="grid">
-                <div class="grid-sizer"></div>
-                <div class="grid-item">
-                    <img src="{{$product->img1}}" />
+                <div class="col">
+                    <h2>{{$product->title}}</h2>
+                    <hr>
+                    <h3>
+                        PrimaVera
+                    </h3>
+                    {{--                <h4>Коллекция:--}}
+                    {{--                    @foreach ($collection as $one_collection)--}}
+                    {{--                    <a href="/collection/{{$one_collection->Collection_Id}}" class="link-dark text-decoration-none text-reset">{{$one_collection->Collection_Name}}. </a>--}}
+                    {{--                    @endforeach--}}
+                    {{--                </h4>--}}
+                    <h5>
+                        Цена: {{$product->price}} ₽
+                        <br>
+                        {{--                    <!-- Цена зак: {{$product->Price}} ₽/{{$product->MainUnit}} -->--}}
+                        {{--                    <!-- <br> {{round(($product->RMPrice/$product->Price)*100 - 100, 2)}}% -->--}}
+                    </h5>
+                    <hr>
+                    {{--                <h5>--}}
+                    {{--                    Остаток: {{$product->balanceCount}} {{$product->MainUnit}}--}}
+                    {{--                </h5>--}}
+                    {{--                <hr>--}}
+                    <h5>
+                        Артикул: {{$product->vendor_code}}
+                    </h5>
+                    <hr>
+                    <h5>В упаковке шт: <strong>{{$product->count_in_pack}}</strong> кв.м:
+                        <strong>{{$product->meters_in_pack}}</strong></h5>
+                    <hr>
+                    {{--                <div id="app-5">--}}
+                    {{--                    <input v-model="count" placeholder="Количество кв.м?"> --}}
+                    {{--                    --}}
+                    {{--                    <h5>@{{ packages }} упаков. общая площадь @{{ all }} кв.м</h5>--}}
+                    {{--                    --}}
+                    {{--                </div>--}}
+                    {{--                <hr>--}}
                 </div>
-
             </div>
-            <hr>
-            <div class="grid">
-                <div class="grid-sizer"></div>
-                @foreach($imgs_2 as $img)
-                <div class="grid-item">
-                    <img src="{{$img}}" />
+            {{--        <!-- <div class="container">--}}
+            {{--                <div class="row" data-masonry='{"percentPosition": true }'>--}}
+            {{--                    <div class="col-sm-6 col-lg-4 mb-4">--}}
+
+            {{--                        @foreach ($url_collection as $url_c)--}}
+            {{--                        <div>--}}
+            {{--                            <img src="/storage/Collections/{{$url_c}}" class="img-fluid shadow p-3 mb-5 bg-white rounded" alt="...">--}}
+            {{--                        </div>--}}
+            {{--                        @endforeach--}}
+
+            {{--                    </div>--}}
+            {{--                </div>--}}
+            {{--            </div> -->--}}
+            {{--        <!-- <hr> -->--}}
+            {{--        <!-- <div class="row" data-masonry='{"percentPosition": true }'>--}}
+            {{--                <div class="col-sm-6 col-lg-4 mb-4">--}}
+            {{--                    <div>--}}
+            {{--                        @foreach ($urls as $url)--}}
+            {{--                        <img src="{{$url}}" class="img-fluid shadow p-3 mb-5 bg-white rounded" alt=".">--}}
+            {{--                        @endforeach--}}
+            {{--                    </div>--}}
+            {{--                </div>--}}
+            {{--            </div> -->--}}
+
+
+            <div class="container-fluid">
+
+
+                <div class="grid">
+                    <div class="grid-sizer"></div>
+                    <div class="grid-item">
+                        <img src="{{$product->img1}}"/>
+                    </div>
+
                 </div>
-                @endforeach
-            </div>
-{{--            <div class="grid">--}}
-{{--                <div class="grid-sizer"></div>--}}
-{{--                @foreach ($urls as $url)--}}
-{{--                <div class="grid-item">--}}
-{{--                    <!-- <img src="{{$url}}" style="border-bottom:1px solid"/> -->--}}
-{{--                    <img src="{{$url}}" />--}}
-{{--                </div>--}}
-{{--                @endforeach--}}
+                <hr>
+                <div class="grid">
+                    <div class="grid-sizer"></div>
+                    @foreach($imgs_2 as $img)
+                        <div class="grid-item">
+                            <img src="{{$img}}"/>
+                        </div>
+                    @endforeach
+                </div>
+                {{--            <div class="grid">--}}
+                {{--                <div class="grid-sizer"></div>--}}
+                {{--                @foreach ($urls as $url)--}}
+                {{--                <div class="grid-item">--}}
+                {{--                    <!-- <img src="{{$url}}" style="border-bottom:1px solid"/> -->--}}
+                {{--                    <img src="{{$url}}" />--}}
+                {{--                </div>--}}
+                {{--                @endforeach--}}
 
-{{--            </div>--}}
+                {{--            </div>--}}
+
+            </div>
+
 
         </div>
-
-
-
-
-
-
     </div>
-</div>
-
 
 @endsection
 
