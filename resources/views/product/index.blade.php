5bf52f54555e4d8438d3d5741e3d7845c07cc448
kkvvnn <kkvvnn89@gmail.com>
2023-10-14 19:09:35 +0300

    addfwefwrfrw

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -74,6 +74,9 @@
                             <div class="card-footer">
                                 <p class="fs-5 text-body-secondary">Цена: {{$product->RMPrice}} ₽/{{$product->MainUnit}} <small class="text-muted"><del>{{$old_price}} </del></small></p>
                                 <hr>
+                                <p class="fs-5 text-body-secondary">Цена -20%: {{round($product->RMPrice * 0.8, -1)}} ₽/{{$product->MainUnit}} <small class="text-muted"><del>{{$old_price}} </del></small></p>
+                                <hr>
+
 
                                 <p class="fs-5 text-body-secondary">Доступно: {{$product->balanceCount}} {{$vivod}}</p>
                                 <hr>
