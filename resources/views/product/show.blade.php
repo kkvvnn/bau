5f0c5167cd93f798c057456d568c600aef309d24
kkvvnn <kkvvnn89@gmail.com>
2023-11-13 18:28:41 +0300

    10%

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -56,11 +56,11 @@
                     <p class="fs-5 text-body-secondary">Цена: {{$product->RMPrice}} ₽/{{$product->MainUnit}} <small class="text-muted"><del>{{$old_price}} </del></small></p>
                     <hr>
                     @if($product->Producer_Brand == 'Laparet' && ($product->RMPriceOld == 0 || $product->RMPriceOld == $product->RMPrice))
-                        <p class="fs-5 text-body-secondary"><button type="button" class="btn btn-info">Цена -15%: {{round($product->RMPrice * 0.85, -1)}} ₽/{{$product->MainUnit}} <small class="text-muted"><del>{{$old_price}} </del></small></button></p>
+                        <p class="fs-5 text-body-secondary"><button type="button" class="btn btn-info">Цена -10%: {{round($product->RMPrice * 0.90, -1)}} ₽/{{$product->MainUnit}} <small class="text-muted"><del>{{$old_price}} </del></small></button></p>
                         <hr>
                     @endif
                     @if($product->Producer_Brand == 'Vitra' && ($product->RMPriceOld == 0 || $product->RMPriceOld == $product->RMPrice))
-                        <p class="fs-5 text-body-secondary"><button type="button" class="btn btn-info">Цена -15%: {{round($product->RMPrice * 0.85, -1)}} ₽/{{$product->MainUnit}} <small class="text-muted"><del>{{$old_price}} </del></small></button></p>
+                        <p class="fs-5 text-body-secondary"><button type="button" class="btn btn-info">Цена -10%: {{round($product->RMPrice * 0.90, -1)}} ₽/{{$product->MainUnit}} <small class="text-muted"><del>{{$old_price}} </del></small></button></p>
                         <hr>
                     @endif
                     @if($product->RMPriceOld && $product->RMPriceOld != $product->RMPrice)
