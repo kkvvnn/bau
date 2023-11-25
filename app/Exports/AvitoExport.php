85010794dec41ee79360bbef7dd7d674c3ce9e5d
kkvvnn <kkvvnn89@gmail.com>
2023-11-25 10:47:31 +0300

    technotile remove all from avito file

--- app/Exports/AvitoExport.php
+++ app/Exports/AvitoExport.php
@@ -91,7 +91,8 @@ class AvitoExport extends DefaultValueBinder implements FromView, WithCustomValu
         $rusplitka = RusplitkaProduct::where([['svoystvo', 'Керамогранит'], ['rest_real_free', '!=', 0]])->get();
 //      ==============================================
 //        $technotile = TechnotileProduct::where('available', 'true')->get();
-        $technotile = TechnotileProduct::where([['available', 'true'], ['price', '>=', 2000]])->get();
+//        $technotile = TechnotileProduct::where([['available', 'true'], ['price', '>=', 2000]])->get();
+        $technotile = [];
 //      ==============================================
         $aquafloor = AquaFloor::where('title', 'not like', '%Подложка%')->get();
 
