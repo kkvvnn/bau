19087b1335b1375039fb368fd75413f0651baa0c
kkvvnn <kkvvnn89@gmail.com>
2023-12-14 17:11:38 +0300

    defrefe

--- app/Console/Commands/BauserviceImport.php
+++ app/Console/Commands/BauserviceImport.php
@@ -11,21 +11,21 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
 
-class UpdateBauservice extends Command
+class BauserviceImport extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'bauservice:update';
+    protected $signature = 'bauservice:import';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Update Bauservice database';
+    protected $description = 'Import and update Bauservice in database';
 
     /**
      * Execute the console command.
