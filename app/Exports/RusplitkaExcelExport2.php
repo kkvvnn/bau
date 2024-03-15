90482881c70b4653645342d2405cb2bb5767ff98
kkvvnn <kkvvnn89@gmail.com>
2024-03-15 12:45:06 +0300

    carving column add

--- app/Exports/RusplitkaExcelExport2.php
+++ app/Exports/RusplitkaExcelExport2.php
@@ -5,12 +5,16 @@ namespace App\Exports;
 use App\Models\Product;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
 use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
+use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Cell\Cell;
 use PhpOffice\PhpSpreadsheet\Cell\DataType;
 use PhpOffice\PhpSpreadsheet\Cell\DefaultValueBinder;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class RusplitkaExcelExport2 extends DefaultValueBinder implements FromView, WithCustomValueBinder
+class RusplitkaExcelExport2 extends DefaultValueBinder implements FromView, WithCustomValueBinder, WithStyles, ShouldAutoSize, WithColumnWidths
 {
     public function bindValue(Cell $cell, $value)
     {
@@ -27,5 +31,29 @@ class RusplitkaExcelExport2 extends DefaultValueBinder implements FromView, With
         return view('exports.rusplitka', [
             'products' => $products,
         ]);
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            // Style the first row as bold text.
+            1    => ['font' => ['bold' => true]],
+
+            // Styling a specific cell by coordinate.
+            'F' => ['font' => ['bold' => true]],
+            'E' => ['font' => ['bold' => true]],
+
+            // Styling an entire column.
+//            'C'  => ['font' => ['size' => 16]],
+        ];
+    }
+
+    public function columnWidths(): array
+    {
+        return [
+            'D' => 15,
+            'E' => 15,
+            'G' => 20,
+        ];
     }
 }
