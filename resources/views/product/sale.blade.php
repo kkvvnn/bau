687521fc743b12fa5ac1082ca85ed9544c0adc7f
kkvvnn <kkvvnn89@gmail.com>
2024-09-25 16:14:48 +0300

    fix slug

--- resources/views/product/sale.blade.php
+++ resources/views/product/sale.blade.php
@@ -33,11 +33,11 @@
                     <div class="col">
                         <div class="card h-100">
                             <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
-                            <a href="/product/{{$product->id}}">
+                            <a href="/product/{{$product->slug}}">
                                 <img src="{{$url1}}" class="card-img-top" alt="...">
                             </a>
                             <div class="card-body">
-                                <a href="/product/{{$product->id}}" class="text-decoration-none text-reset">
+                                <a href="/product/{{$product->slug}}" class="text-decoration-none text-reset">
                                     <h5 class="card-title">{{$product->Producer_Brand}} {{$product->Name}}</h5>
                                 </a>
                                 <p class="card-text"></p>
