c0a6a92b8d5b6e0b4723ec2782a67a87895baa34
kkvvnn <kkvvnn89@gmail.com>
2024-10-24 18:06:24 +0300

    Contacts in progress 2

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -8,7 +8,7 @@
 
             <div class="row">
                 <div class="col">
-                    <p>123</p>
+                    <a href="https://yandex.ru/maps/-/CDdTIJzQ">Точка на карте</a>
                 </div>
                 <div class="col">
                     <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A7ddde8cb02cc9fbbcb20285ee41a8e5fff22ddc63fd72e03dce8ef16787e8b22&amp;width=853&amp;height=720&amp;lang=ru_RU&amp;scroll=true"></script>
