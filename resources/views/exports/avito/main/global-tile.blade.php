9245cb46d52053b6c73a99622be43566f87b2667
kkvvnn <kkvvnn89@gmail.com>
2024-11-23 15:13:28 +0300

    GlobalTile keramogranit brand name

--- resources/views/exports/avito/main/global-tile.blade.php
+++ resources/views/exports/avito/main/global-tile.blade.php
@@ -9,7 +9,7 @@
                 $GoodsSubType = 'Отделка';
                 $FinishingMaterialsType = 'Керамическая плитка и керамогранит';
                 $CeramicPorcelainTilesSubType = 'Керамогранит';
-                $Brand = 'Global Tile';
+                $Brand = 'GlobalTile';
                 $TileType = '';
                 $SpaceType = '';
                 $InstallationType = avito_bauservice_for('На пол | На стену');
