d5ee1317e5197bb075ac81357a9ae1ebb7011766
kkvvnn <kkvvnn89@gmail.com>
2024-12-13 11:37:41 +0300

    fdgeg

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-         $schedule->command('app:import')->dailyAt('10:35');
+         $schedule->command('app:import')->dailyAt('05:30');
     }
 
     /**
