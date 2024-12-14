7a0e5bb22cdd45df5cb5f2d0839892fb7018e993
kkvvnn <kkvvnn89@gmail.com>
2024-12-14 17:54:43 +0300

    fix igalgo width

--- resources/views/exports/avito/millennium/idalgo.blade.php
+++ resources/views/exports/avito/millennium/idalgo.blade.php
@@ -8,8 +8,8 @@
         $TileType = '';
         $SpaceType = '';
         $InstallationType = avito_bauservice_for('На пол | На стену');
-        $Width = avito_bauservice_size(null, 5, 200, $product->title, 'W');
-        $Length = avito_bauservice_size(null, 5, 400, $product->title, 'L');
+        $Width = avito_bauservice_size(null, 5, 200, $product->title, 'W')/10;
+        $Length = avito_bauservice_size(null, 5, 400, $product->title, 'L')/10;
         $Height = avito_bauservice_height($product->fat, 2, 30);
         $Pattern = avito_bauservice_pattern($product->title, '');
         $Color = avito_bauservice_color($product->color??'');
