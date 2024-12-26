75e6e39d6a6e466e710a884212e352ec619da42e
kkvvnn <kkvvnn89@gmail.com>
2024-12-26 15:00:33 +0300

    drgtege5 dffrrver

--- resources/views/exports/avito/main/main.blade.php
+++ resources/views/exports/avito/main/main.blade.php
@@ -42,18 +42,18 @@
     </thead>
     <tbody>
 
-{{--        @include('exports.avito.main.bauservice')  //READY--}}
-{{--        @include('exports.avito.main.global-tile') //READY--}}
-{{--        @include('exports.avito.main.kerranova') //READY--}}
-{{--        @include('exports.avito.main.primavera') //READY--}}
-{{--        @include('exports.avito.main.leedo') //READY--}}
-{{--        @include('exports.avito.main.artkera') //READY--}}
+        @include('exports.avito.main.bauservice')  //READY
+        @include('exports.avito.main.global-tile') //READY
+        @include('exports.avito.main.kerranova') //READY
+        @include('exports.avito.main.primavera') //READY
+        @include('exports.avito.main.leedo') //READY
+        @include('exports.avito.main.artkera') //READY
 
-{{--        @include('exports.avito.main.rusplitka') //READY--}}
-{{--        @include('exports.avito.main.aquafloor') //READY--}}
-{{--        @include('exports.avito.main.pixmosaic') //READY--}}
-{{--        @include('exports.avito.main.artcenter') //READY--}}
-{{--        @include('exports.avito.main.skalla') //READY--}}
+        @include('exports.avito.main.rusplitka') //READY
+        @include('exports.avito.main.aquafloor') //READY
+        @include('exports.avito.main.pixmosaic') //READY
+        @include('exports.avito.main.artcenter') //READY
+        @include('exports.avito.main.skalla') //READY
         @include('exports.avito.main.nt-ceramic-hand')
         @include('exports.avito.main.primavera-hand')
         @include('exports.avito.main.primavera-hand-2')
