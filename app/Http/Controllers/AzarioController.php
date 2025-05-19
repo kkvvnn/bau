76e9d93dac7d9b247ea1c8b3176d0ab09832cfb3
kkvvnn <kkvvnn89@gmail.com>
2025-05-19 17:10:28 +0300

    fdefg rgfe greg

--- app/Http/Controllers/AzarioController.php
+++ app/Http/Controllers/AzarioController.php
@@ -8,6 +8,7 @@ use App\Models\Azario;
 use App\Models\AzarioPriceStock;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Maatwebsite\Excel\Facades\Excel;
 
 class AzarioController extends Controller
@@ -51,10 +52,60 @@ class AzarioController extends Controller
         $products = Azario::whereHas('props', function ($query) {
             $query->where('price', '!=', 0);
         })
-            ->orderByRaw('length * width DESC')
+            ->orderByDesc('length')
+            ->orderByDesc('width')
             ->paginate(15);
 
 
         return view('azario.index', compact('products'));
+    }
+
+    public function show($slug)
+    {
+        $product = Azario::whereSlug($slug)->firstOrFail();
+
+        $string_for_delete = 'https://www.santehcentr.com';
+        $img = Storage::disk('azario')->url(Str::remove($string_for_delete, $product->images[0]));
+
+//        -----------------------------
+        $urls_c = [];
+        if ($product->images != '') {
+            $urls_c[] = Storage::disk('azario')->url(Str::remove($string_for_delete, $product->images[0]));
+        } else {
+            $urls_c[] = Storage::disk('no_image')->url('no_image.jpg');
+        }
+//        -----------------------------------
+
+        $urls_2 = [];
+        foreach ($product->images as $key => $value) {
+            $urls_2[] = Storage::disk('azario')->url(Str::remove($string_for_delete, $value));
+        }
+//        ------------------------------------
+
+        $text_color = '';
+        $date_now = \Carbon\Carbon::now();
+        $date_of_update = $product->props->updated_at;
+        $diff_days = $date_now->diffInDays($date_of_update);
+
+        if ($diff_days == 0) {
+            $text_color = 'text-success';
+        } elseif ($diff_days <= 7) {
+            $text_color = 'text-warning';
+        } else {
+            $text_color = 'text-danger';
+        }
+
+        $vivod = '';
+
+//        return view('pixmosaic-new.show', compact('product', 'text_color', 'urls_c'));
+        return view('azario.show', [
+            'product' => $product,
+            'urls' => $urls_2,
+            // 'url2' => $url2,
+//            'collection' => $collection,
+            'url_collection' => $urls_c,
+            'vivod' => $vivod,
+            'text_color' => $text_color,
+        ]);
     }
 }
