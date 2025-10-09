512dedcffe58626b10fd7baefa7505a47c72257f
kkvvnn <kkvvnn89@gmail.com>
2025-10-09 14:24:47 +0300

    dsaf ef fdf

--- resources/views/help/list.blade.php
+++ resources/views/help/list.blade.php
@@ -9,11 +9,14 @@
         <th style="border: 1px solid black;border-collapse: collapse;">Бренд</th>
         <th style="border: 1px solid black;border-collapse: collapse;">Название</th>
         <th style="border: 1px solid black;border-collapse: collapse;">РРЦ</th>
+        <th style="border: 1px solid black;border-collapse: collapse;">На складе</th>
+        <th style="border: 1px solid black;border-collapse: collapse;">Ед.изм.</th>
         <th style="border: 1px solid black;border-collapse: collapse;">Формат</th>
         <th style="border: 1px solid black;border-collapse: collapse;">Толщина</th>
         <th style="border: 1px solid black;border-collapse: collapse;">Поверхность</th>
         <th style="border: 1px solid black;border-collapse: collapse;">Цвет</th>
         <th style="border: 1px solid black;border-collapse: collapse;">Дизайн</th>
+        <th style="border: 1px solid black;border-collapse: collapse;">Распродажа</th>
     </tr>
 @foreach ($products as $product)
 
@@ -25,11 +28,19 @@
 {{--        <td>{{$product->Element_Code}}</td>--}}
         <td style="border: 1px solid black;border-collapse: collapse; padding-left: 5px; padding-right: 5px">{{$product->Name}}</td>
         <td style="border: 1px solid black;border-collapse: collapse; padding-left: 5px; padding-right: 5px">{{$product->RMPrice}}</td>
+        <td style="border: 1px solid black;border-collapse: collapse; padding-left: 5px; padding-right: 5px">{{$product->balanceCount}}</td>
+        <td style="border: 1px solid black;border-collapse: collapse; padding-left: 5px; padding-right: 5px">{{$product->MainUnit}}</td>
         <td style="border: 1px solid black;border-collapse: collapse; padding-left: 5px; padding-right: 5px">{{$product->Height}}x{{$product->Lenght}}</td>
         <td style="border: 1px solid black;border-collapse: collapse; padding-left: 5px; padding-right: 5px">{{$product->Thickness}}</td>
         <td style="border: 1px solid black;border-collapse: collapse; padding-left: 5px; padding-right: 5px">{{$product->Surface}}</td>
         <td style="border: 1px solid black;border-collapse: collapse; padding-left: 5px; padding-right: 5px">{{$product->Color}}</td>
         <td style="border: 1px solid black;border-collapse: collapse; padding-left: 5px; padding-right: 5px">{{$product->DesignValue}}</td>
+
+        @if(($product->RMPriceOld && $product->RMPriceOld != $product->RMPrice))
+            <td style="border: 1px solid black;border-collapse: collapse; padding-left: 5px; padding-right: 5px">Распродажа</td>
+        @else
+            <td style="border: 1px solid black;border-collapse: collapse; padding-left: 5px; padding-right: 5px"></td>
+        @endif
     </tr>
 
 
