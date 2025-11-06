2bdf1a187189c6ac03241d98cb9576bd1e023aaf
kkvvnn <kkvvnn89@gmail.com>
2025-11-06 11:58:30 +0300

    dwerf frew

--- app/Http/Controllers/AvitoController.php
+++ app/Http/Controllers/AvitoController.php
@@ -110,8 +110,8 @@ class AvitoController extends Controller
 //                $data['address'] = 'Москва, Филёвская линия, метро Фили';
 //                $data['address'] = 'Москва, Можайское шоссе, 42';
                 $data['communication'] = [
-                    'phone_and_message' => '',
-                    'only_message' => 'selected',
+                    'phone_and_message' => 'selected',
+                    'only_message' => '',
                     'only_phone' => '',
                 ];
                 break;
