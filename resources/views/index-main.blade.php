e38b86844a89f16299d37e8be0de64677f47c43f
kkvvnn <kkvvnn89@gmail.com>
2025-12-02 23:29:20 +0300

    dsfg gre

--- resources/views/index-main.blade.php
+++ resources/views/index-main.blade.php
@@ -32,7 +32,7 @@
 
             <div class="px-4 py-5 my-5 text-center">
 {{--                <img class="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">--}}
-                <h1 class="display-5 fw-bold text-body-emphasis">Керамогранит, мозаика, ламинат и многое другое в Москве, <span class="text-nowrap">Санк-Петербурге</span> и Казани</h1>
+                <h1 class="display-5 fw-bold text-body-emphasis">Керамогранит, мозаика, ламинат и многое другое в Москве, <span class="text-nowrap">Санк-Петербурге</span>, Казани</h1>
                 <div class="col-lg-6 mx-auto">
                     <p class="lead mb-4">Мы - Официальные дилеры</p>
 
