ba2c516a99f5434030ab3352d53fad5231694224
kkvvnn <kkvvnn89@gmail.com>
2026-08-10 11:27:30 +0300

    slug fix 2

--- resources/views/product/sale.blade.php
+++ resources/views/product/sale.blade.php
@@ -56,11 +56,11 @@
                     <div class="col">
                         <div class="card h-100">
                             <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
-                            <a href="/product/{{$product->originals->slug}}">
+                            <a href="/product/{{$slug}}">
                                 <img src="{{$url1}}" class="card-img-top" alt="...">
                             </a>
                             <div class="card-body">
-                                <a href="/product/{{$product->originals->slug}}" class="text-decoration-none text-reset">
+                                <a href="/product/{{$slug}}" class="text-decoration-none text-reset">
                                     <h5 class="card-title">{{$product->Producer_Brand}} {{$product->Name}}</h5>
 
                                     @if($product->Name != $product->originals->Name)
