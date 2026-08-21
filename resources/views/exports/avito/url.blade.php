7a46d0b76cb0917bca16d6a0362d1d51cbaaf390
kkvvnn <kkvvnn89@gmail.com>
2026-08-21 11:10:43 +0300

    fds

--- resources/views/exports/avito/url.blade.php
+++ resources/views/exports/avito/url.blade.php
@@ -27,6 +27,14 @@
             <hr>
             <h3>{{ $url }}</h3>
 
+            <!-- Блок с кнопками Bootstrap -->
+            <div class="d-flex gap-2">
+                <!-- Кнопка Скачать (просто ссылка со стилем кнопки и атрибутом download) -->
+                <a href="{{ $url }}" class="btn btn-success" download>
+                    Скачать
+                </a>
+            </div>
+
         </div>
     </div>
 
